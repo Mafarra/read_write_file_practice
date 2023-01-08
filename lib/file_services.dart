4eb95b22f56f3e195a6ccf0c54e1c052d4b88f1c
Mammdouh Al-farra <mam.farra2030@gmail.com>
2023-01-08 15:34:10 +0200

    more focused functions for read and write files.

--- lib/file_services.dart
+++ lib/file_services.dart
@@ -12,31 +12,29 @@ class FileService {
   }
 
   Future<void> readWriteFile(String fileName) async {
-    // Get the directory path for both Android and iOS to store file.
     final Directory dir = await getApplicationDocumentsDirectory();
     final String path = '${dir.path}/$fileName';
+    final File file = File(path);
 
-    // Attempt to read the file
-    File file;
-    try {
-      file = File(path);
-      String contents = await file.readAsString();
-      print(contents);
-    } catch (e) {
-      // If encountering an error, return
-      print("Error reading file: $e");
-      return;
-    }
-
-    // If the file doesn't exist, create it and write to it
     if (!await file.exists()) {
-      file = await file.create();
-      await file.writeAsString('Hello World!', mode: FileMode.write);
-      print("File created and written to");
+      await _createAndWriteToFile(file);
     } else {
-      // Otherwise, truncate the file and write to it
-      await file.writeAsString('Hello World!', mode: FileMode.writeOnly);
-      print("File truncated and written to");
+      await _truncateAndWriteToFile(file);
     }
   }
+
+  Future<void> _createAndWriteToFile(File file) async {
+    file = await file.create();
+    await file.writeAsString('Hello World!', mode: FileMode.write);
+    print("File created and written to");
+  }
+
+  Future<void> _truncateAndWriteToFile(File file) async {
+    await file.writeAsString('Hello World!', mode: FileMode.writeOnly);
+    print("File truncated and written to");
+  }
+//This version of the code has the readWriteFile function
+// delegate the creation and writing to the file to the
+// _createAndWriteToFile function, and the truncation and writing
+// to the file to the _truncateAndWriteToFile function.
 }
